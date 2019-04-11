585365191d703d28a920b7cc3cad8da822f11163
Michel GILLET <michel@devatom.net>
2019-04-11 09:25:16 +0200

    Correction ajax

--- includes/controllers/controller_ajax.php
+++ includes/controllers/controller_ajax.php
@@ -17,64 +17,54 @@
 				$mail->CharSet = 'utf-8';
 				// Create the email object
 				try {
-					//Server settings
-					$mail->SMTPDebug = 0;                                 // Enable verbose debug output
-					$mail->isSMTP();                                      // Set mailer to use SMTP
-					$mail->Host = $smtpParams['host'];  // Specify main and backup SMTP servers
-					$mail->Port = $smtpParams['port'];                                    // TCP port to connect to
-					$mail->Helo = $smtpParams['helo'];
-					if ($smtpParams['auth']){
-						$mail->SMTPAuth = $smtpParams['auth'];                               // Enable SMTP authentication
-						$mail->Username = $smtpParams['user'];                 // SMTP username
-						$mail->Password = $smtpParams['pass'];                           // SMTP password
-						$mail->SMTPAutoTLS = true;
-						$mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
-						$mail->SMTPOptions =
-							array(
-								"ssl" => array(
-									"verify_peer" => false,
-									"verify_peer_name" => false,
-									"allow_self_signed" => true
-								)
-							);
+					if (isset($smtpParams)){
+						//Server settings
+						$mail->SMTPDebug = 0;                                 // Enable verbose debug output
+						$mail->isSMTP();                                      // Set mailer to use SMTP
+						$mail->Host = $smtpParams['host'];                // Specify main and backup SMTP servers
+						$mail->Port = $smtpParams['port'];                                    // TCP port to connect to
+						$mail->Helo = $smtpParams['helo'];
+						if ($smtpParams['auth']){
+							$mail->SMTPAuth = $smtpParams['auth'];                               // Enable SMTP authentication
+							$mail->Username = $smtpParams['user'];                 // SMTP username
+							$mail->Password = $smtpParams['pass'];                           // SMTP password
+							$mail->SMTPAutoTLS = true;
+							$mail->SMTPSecure = 'TLS';                            // Enable TLS encryption, `ssl` also accepted
+							$mail->SMTPOptions = array("ssl" => array("verify_peer" => false, "verify_peer_name" => false, "allow_self_signed" => true));
+						}
+						//Recipients
+						$mail->setFrom('studynet@devatom.net', APPNAME);
+						$mail->addAddress($personne->getEmail(), $personne->getNomComplet());     // Add a recipient
+						//Content
+						$mail->isHTML(true);                                  // Set email format to HTML
+						$mail->Subject = "Vos informations d'accès à la plateforme " . APPNAME;
+						$messageHtml = "<html><body>Bonjour,<br /> vous recevez ce message de la part de la plateforme " . APPNAME . ".<br />";
+						$messageHtml .= 'Pour vous connecter : <a href="' . ROOTHTML . '" title="' . APPNAME . '">' . APPNAME . '</a><br />';
+						if (!$personne->getUserAuth()->getId() == 0){
+							$messageHtml .= 'Votre identifiant : ' . $personne->getUserAuth()->getLogin() . '<br />';
+							$messageHtml .= 'Votre mot de passe : ' . $personne->getUserAuth()->getPassword() . '<br />';
+						}else{
+							$messageHtml .= "Vos information d'authentification ne sont pas encore enregistrées.<br />";
+							$messageHtml .= "N'hésitez pas à nous demander de les créer pour que vous puissiez accéder à l'application.<br />";
+						}
+						$messageHtml .= 'Votre profil est ' . $personne->getNomComplet() . ' => ' . $personne->get_class() . '<br /><br />';
+						$messageHtml .= "Cordialement</body></html>";
+						$messageText = "Bonjour,\r\n vous recevez ce message de la part de la plateforme " . APPNAME . ".\r\n";
+						$messageText .= 'Pour vous connecter : ' . ROOTHTML . "\r\n";
+						if (!$personne->getUserAuth()->exists()){
+							$messageText .= 'Votre identifiant : ' . $personne->getUserAuth()->getLogin() . "\r\n";
+							$messageText .= 'Votre mot de passe : ' . $personne->getUserAuth()->getPassword() . "\r\n";
+						}else{
+							$messageText .= "Vos information d'authentification ne sont pas encore enregistrées.<br />";
+							$messageText .= "N'hésitez pas à nous demander de les créer pour que vous puissiez accéder à l'application.<br />";
+						}
+						$messageText .= 'Votre profil est ' . $personne->getNomComplet() . ' => ' . $personne->get_class() . "\r\n\r\n";
+						$messageText .= "Cordialement.";
+						$mail->Body = $messageHtml;
+						$mail->AltBody = $messageText;
+						$mail->send();
+						print 'Message envoyé';
 					}
-
-					//Recipients
-					$mail->setFrom('studynet@devatom.net', APPNAME);
-					$mail->addAddress($personne->getEmail(), $personne->getNomComplet());     // Add a recipient
-
-					//Content
-					$mail->isHTML(true);                                  // Set email format to HTML
-					$mail->Subject = "Vos informations d'accès à la plateforme ".APPNAME;
-					$messageHtml = "<html><body>Bonjour,<br /> vous recevez ce message de la part de la plateforme ".APPNAME.".<br />";
-					$messageHtml .= 'Pour vous connecter : <a href="'.ROOTHTML.'" title="'.APPNAME.'">'.APPNAME.'</a><br />';
-					if (!$personne->getUserAuth()->getId() == 0){
-						$messageHtml .= 'Votre identifiant : '.$personne->getUserAuth()->getLogin().'<br />';
-						$messageHtml .= 'Votre mot de passe : '.$personne->getUserAuth()->getPassword().'<br />';
-					}else{
-						$messageHtml .= "Vos information d'authentification ne sont pas encore enregistrées.<br />";
-						$messageHtml .= "N'hésitez pas à nous demander de les créer pour que vous puissiez accéder à l'application.<br />";
-					}
-					$messageHtml .= 'Votre profil est '.$personne->getNomComplet().' => '.$personne->get_class().'<br /><br />';
-					$messageHtml .= "Cordialement</body></html>";
-
-					$messageText = "Bonjour,\r\n vous recevez ce message de la part de la plateforme ".APPNAME.".\r\n";
-					$messageText .= 'Pour vous connecter : '.ROOTHTML."\r\n";
-					if (!$personne->getUserAuth()->exists()){
-						$messageText .= 'Votre identifiant : '.$personne->getUserAuth()->getLogin()."\r\n";
-						$messageText .= 'Votre mot de passe : '.$personne->getUserAuth()->getPassword()."\r\n";
-					}else{
-						$messageText .= "Vos information d'authentification ne sont pas encore enregistrées.<br />";
-						$messageText .= "N'hésitez pas à nous demander de les créer pour que vous puissiez accéder à l'application.<br />";
-					}
-					$messageText .= 'Votre profil est '.$personne->getNomComplet().' => '.$personne->get_class()."\r\n\r\n";
-					$messageText .= "Cordialement.";
-
-					$mail->Body = $messageHtml;
-					$mail->AltBody = $messageText;
-
-					$mail->send();
-					print 'Message envoyé';
 				} catch (Exception $e) {
 					print 'Message non envoyé.<br />';
 					print 'Erreur : ' . $mail->ErrorInfo;
